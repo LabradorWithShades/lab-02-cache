fedf9abffa2ad6e35f5b4adf130ba4d70b5361bb
LabradorWithShades <70892548+LabradorWithShades@users.noreply.github.com>
2020-10-31 18:40:11 +0300

    nvm

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nikolai/Documents/GitHub/lab-02-cache
-# Build directory: /home/nikolai/Documents/GitHub/lab-02-cache/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/nikolai/Documents/GitHub/lab-02-cache/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/nikolai/Documents/GitHub/lab-02-cache/CMakeLists.txt;49;add_test;/home/nikolai/Documents/GitHub/lab-02-cache/CMakeLists.txt;0;")
